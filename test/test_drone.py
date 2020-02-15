e21314125a2c82d05617fb33773e3e15322ac273
Hell <luis.manuel.angueira@gmail.com>
2020-02-15 22:10:05 +0100

    Added Unit Tests for movement and  Batch movement

--- test/test_drone.py
+++ test/test_drone.py
@@ -7,6 +7,26 @@ from drone.drone import Drone
 
 class TestDrone(TestCase):
 
+    def test_move_forward_N(self):
+        drone = Drone(0, 0, "N")
+        drone.move("M")
+        self.assertEqual(1, drone.position_x)
+
+    def test_move_forward_E(self):
+        drone = Drone(0, 0, "E")
+        drone.move("M")
+        self.assertEqual(1, drone.position_y)
+
+    def test_move_L(self):
+        drone = Drone(0, 0, "N")
+        drone.move("L")
+        self.assertEqual("W", drone.orientation)
+
+    def test_move_R(self):
+        drone = Drone(0, 0, "N")
+        drone.move("R")
+        self.assertEqual("E", drone.orientation)
+
     def test_rotate_clockwise_N(self):
         drone = Drone(0, 0, "N")
         drone._rotate_clockwise()
@@ -66,3 +86,22 @@ class TestDrone(TestCase):
         drone = Drone(2, 2, "W")
         drone._move_forward()
         self.assertEqual(1, drone.position_x)
+
+    def test_current_position_incorrect(self):
+        drone = Drone(2, 2, "W")
+        self.assertNotEqual("22W", drone.current_position)
+
+    def test_move_drone_without_area_simple_movement(self):
+        drone = Drone(3, 3, "E")
+        instructions = "L"
+        self.assertEqual("3 3 N", drone.move_with_instructions(instructions))
+
+    def test_move_drone_without_area_middle(self):
+        drone = Drone(3, 3, "E")
+        instructions = "MMRMMRMRRM"
+        self.assertEqual("5 1 E", drone.move_with_instructions(instructions))
+
+    def test_move_drone_without_area_complex(self):
+        drone = Drone(1, 2, "N")
+        instructions = "LM LM LM LM ML ML ML ML MM"
+        self.assertEqual("1 4 N", drone.move_with_instructions(instructions))
