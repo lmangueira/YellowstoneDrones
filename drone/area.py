a69544742807137eeaf5590e10c575deca2e5e4b
Hell <luis.manuel.angueira@gmail.com>
2020-02-19 21:18:12 +0100

    Fixed Drone area initialization, where we were checking a non existent value

--- drone/area.py
+++ drone/area.py
@@ -1,20 +1,20 @@
 class Area:
 
-    def __init__(self, height, width):
+    def __init__(self, height: int, width: int):
         """
         Area init
         :param height: Height - must be greater than zero
         :param width: Width - must be greater than zero
         """
-        if self.height < 1:
+        if height < 1:
             raise ValueError(f"Area height must be greater than zero. Value received {height}")
-        if self.width < 1:
+        if width < 1:
             raise ValueError(f"Area width must be greater than zero. Value received {width}")
 
         self.height = height
         self.width = width
 
-    def valid_movement(self, position_x, position_y):
+    def valid_movement(self, position_x: int, position_y: int):
         """
         Checks whether a movement is valid or not.
         A valid movement is the one in the bounds.
